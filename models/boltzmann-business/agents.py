aa71cfdde32a92f109ff2396491a51be46a35bd7
Fill Staley <fillstaley@gmail.com>
2025-03-18 20:23:31 -0700

    fleshed out the beginnings of the employment system

--- models/boltzmann-business/agents.py
+++ models/boltzmann-business/agents.py
@@ -26,7 +26,22 @@ class Individual(employment.Employee, banking.Agent, mesa.Agent):
     ##############
     
     def act(self) -> None:
-        pass
+        
+        # process reviewed employment applications
+        for app in self.reviewed_employment_applications:
+            if app.approved and not self._current_employment_contracts:
+                if contract := app.accept():
+                    self._current_employment_contracts.append(contract)
+            self._open_employment_applications.remove(app)
+            self._closed_employment_applications.append(app)
+        
+        # if unemployed, search and apply for open jobs
+        if not self._current_employment_contracts and getattr(self.model, "job_board", None):
+            sample_size = min(3, len(self.model.job_board))
+            jobs_sample = self.random.sample(self.model.job_board, sample_size)
+            for job in jobs_sample:
+                if application := job.apply(self):
+                    self._open_employment_applications.append(application)
 
     ###########
     # Methods #
@@ -39,19 +54,48 @@ class Individual(employment.Employee, banking.Agent, mesa.Agent):
 class Business(employment.Employer, banking.Agent, mesa.Agent):
     def __init__(self, model, *args, **kwargs) -> None:
         super().__init__(model=model, *args, **kwargs)
-    
+        
+        self.employment_apps_review_limit: int | None = None
     
     ##############
     # Properties #
     ##############
-    
+
     
     ##############
     # Act Method #
     ##############
     
     def act(self) -> None:
-        pass
+        
+        self.employment_apps_reviewed = 0
+        
+        open_jobs = self.open_jobs
+        self.random.shuffle(open_jobs)
+        
+        # loop over the open jobs and review as many applications as possible
+        for job in open_jobs:
+            # each job has only so many open positions, 
+            # we don't want to offer more jobs than are available
+            offers_available = job.open_positions
+            while (
+                (
+                    self.employment_apps_review_limit is None
+                    or self.employment_apps_reviewed < self.employment_apps_review_limit
+                )
+                and offers_available > 0
+                and (application := self.next_employment_application(job))
+            ):
+                self.employment_apps_reviewed += 1
+                
+                if self.random.random() < self.approval_probability:
+                    application.approve()
+                    offers_available -= 1
+                else:
+                    application.deny()
+            
+            if self.employment_apps_reviewed >= self.employment_apps_review_limit:
+                break
 
     ###########
     # Methods #
--- models/boltzmann-business/employment.py
+++ models/boltzmann-business/employment.py
@@ -3,12 +3,183 @@
 
 
 from __future__ import annotations
+from collections import deque
 
 class Employee:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        self._open_employment_applications: list[EmploymentApplication] = []
+        self._closed_employment_applications: list[EmploymentApplication] = []
+        
+        self._current_employment_contracts: list[EmploymentContract] = []
+        self._past_employment_contracts: list[EmploymentContract] = []
+    
+    @property
+    def reviewed_employment_applications(self) -> list[EmploymentApplication]:
+        return [a for a in self._open_employment_applications if not a.pending]
 
 
 class Employer:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        self.payroll: dict[Employee, EmploymentContract] = {}
+        
+        self._received_job_applications: dict[Job, deque[EmploymentApplication]] = {}
+    
+    ##############
+    # Properties #
+    ##############
+    
+    @property
+    def employees(self) -> list[Employee]:
+        return list(self.payroll.keys())
+    
+    @property
+    def open_jobs(self) -> list[Job]:
+        return list(self._received_job_applications.keys())
+    
+    ###########
+    # Methods #
+    ###########
+    
+    def begin_hiring(self, job: Job) -> bool:
+        """Initializes a queue for receiving applications for an open job."""
+        if not job.open_positions:
+            return False
+        
+        if job in self._received_job_applications:
+            return False
+        
+        # we initialize an empty queue for applications
+        self._received_job_applications[job] = deque()
+        return True
+    
+    def receive_employment_application(self, application: EmploymentApplication):
+        job = application.job
+        if job in self._received_job_applications:
+            self._received_job_applications[job].append(application)
+    
+    def next_employment_application(self, job: Job) -> EmploymentApplication | None:
+        if job not in self._received_job_applications:
+            return None
+        
+        if not (queue := self._received_job_applications[job]):
+            return None
+        return queue.popleft()
+    
+    def end_hiring(self, job: Job) -> bool:
+        """Denies all remaining applications for a job and deletes the queue."""
+        if job.open_positions:
+            return False
+        
+        if job not in self._received_job_applications:
+            return False
+        
+        # we clear the queue of applications then remove it
+        while application := self.next_employment_application(job):
+            application.deny()
+        del self._received_job_applications[job]
+        return True
+    
+    def hire(self, application: EmploymentApplication, date: int) -> EmploymentContract | None:
+        """Adds an employee to the payroll.
+
+        Parameters
+        ----------
+        application : JobApplication
+            An approved employment application.
+        date : int
+            The date on which the applicant is hired.
+
+        Returns
+        -------
+        Job | None
+            If the application is approved, and the applicant is not already an
+            employee, then a new Job instance is returned; otherwise None is returned.
+        """
+        if application.approved:
+            contract = EmploymentContract()
+            self.payroll[application.applicant] = contract
+            return contract
+        return None
+    
+    
+    def fire(self, employee: Employee, date: int) -> bool:
+        """Removes an employee from the payroll.
+
+        Parameters
+        ----------
+        employee : Employee
+            The employee to be fired.
+        date : int
+            The date on which the employee is fired.
+        """
+        if employee in self.payroll:
+            del self.payroll[employee]
+            return True
+        return False
+
+
+class Job:
+    def __init__(self, employer: Employer, *employees: Employee, max_employees: int = 1):
+        self.employer = employer
+        self.employees = list(employees)
+        self.max_employees = max_employees
+    
+    @property
+    def open_positions(self) -> bool:
+        return self.max_employees - len(self.employees)
+    
+    def apply(self, applicant: Employee) -> EmploymentApplication | None:
+        if self.open_positions:
+            application = EmploymentApplication(applicant, job=self)
+            self.employer.receive_employment_application(application)
+            return application
+        return None
+
+
+class EmploymentApplication:
+    def __init__(self, applicant: Employee, job: Job) -> None:
+        self.applicant = applicant
+        self.job = job
+        
+        # _approved can be True (approved), False (denied), or None (pending)
+        self._approved: bool | None = None
+    
+    @property
+    def employer(self) -> Employer:
+        return self.job.employer
+
+    @property
+    def pending(self) -> bool:
+        """Returns True if the application has been neither approved nor denied."""
+        return self._approved is None
+
+    @property
+    def approved(self) -> bool:
+        """Returns True if the application has been approved."""
+        return self._approved is True
+    
+    @property
+    def denied(self) -> bool:
+        """Returns True if the application has been denied."""
+        return self._approved is False
+    
+    def approve(self):
+        """Changes the application from pending to approved."""
+        if self._approved is None:
+            self._approved = True
+    
+    def deny(self):
+        """Changes the application from pending to denied."""
+        if self._approved is None:
+            self._approved = False
+    
+    def accept(self) -> EmploymentContract:
+        return self.employer.hire(self)
+
+
+class EmploymentContract:
+    pass
