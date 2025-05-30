8e1061502b816cc9649f78835cef18c8247e3d7f
Fill Staley <fillstaley@gmail.com>
2025-05-29 17:22:20 -0700

    began sketching out the logic for Depositor

--- src/econolab/financial/deposits/agents/depositor.py
+++ src/econolab/financial/deposits/agents/depositor.py
@@ -16,4 +16,49 @@ class Depositor(Creditor):
     ...
     """
     
-    pass
+    __slots__ = (
+        "_open_deposit_applications",
+    )
+    _open_deposit_applications: list
+    
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        
+        self._open_deposit_applications = []
+    
+    
+    ###########
+    # Actions #
+    ###########
+    
+    def open_deposit_account(self):
+        if offers := [
+            app for app in self._open_deposit_applications if app.reviewed
+        ]:
+            self.prioritize_deposit_offers(offers)
+            if accepted := self._respond_to_deposit_offers(*offers):
+                return accepted
+        # Otherwise, search for and apply to new options
+        return self._apply_for_deposit_accounts()
+    
+    def close_deposit_account(self):
+        pass
+    
+    
+    #########
+    # Hooks #
+    #########
+    
+    def prioritize_deposit_offers(self, offers):
+        pass
+    
+    
+    ##############
+    # Primitives #
+    ##############
+    
+    def _apply_for_deposit_accounts(self):
+        pass
+    
+    def _respond_to_deposit_offers(self, *args):
+        pass
